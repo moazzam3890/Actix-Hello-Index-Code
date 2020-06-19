488a64ca09da26ebf9e69f3ae62d374763a31393
Moazzam Adil Khan <moaadi3890@gmail.com>
2020-06-19 23:26:41 +0500

    comments added

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 use actix_web::{web, App, HttpServer,Responder, HttpResponse};
 
-async fn index_0()-> impl Responder{
+async fn index_0()-> impl Responder{ //Request handler
     "Hello Index 0"
 }
 
-async fn index_1() -> impl Responder {
-    HttpResponse::Ok().body("Hello Index 1")
+async fn index_1() -> impl Responder { //Request handler
+    HttpResponse::Ok().body("Hello Index 1") 
 }
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(||{
-        App::new()
-        .route("/", web::get().to(index_0))
-        .service(
-            web::scope("/index1")
-            .route("/hello", web::get().to(index_1))
+        App::new() //create new application builder
+        .route("/", web::get().to(index_0))//configure index_0 (route) for specific path ("/")
+        .service( //Register http service 
+            web::scope("/index1") //set of resources with common route path
+            .route("/hello", web::get().to(index_1)) //
         )
     })
-    .bind("127.0.0.1:8088")?
-    .run() 
+    .bind("127.0.0.1:8088")? // bind to the local host
+    .run() // start new thread and start listning for incoming connections
     .await //Executor to wait for async function to end and return a future
 }
